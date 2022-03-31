4057b1af0b607835551968ef3b9a267b62432ec1
Hope-Parnell <parnell.hope.c@gmail.com>
2022-03-31 09:53:42 -0500

    Added timer and WinFlag

---
+++ 0x00-unity-assets_models_textures/Assets/Scripts/WinTrigger.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinTrigger : MonoBehaviour
+{
+    public Text TimerText;
+    private void OnTriggerEnter(Collider other){
+        other.GetComponent<Timer>().enabled = false;
+        TimerText.color = Color.green;
+        TimerText.fontSize = 60;
+    }
+}
