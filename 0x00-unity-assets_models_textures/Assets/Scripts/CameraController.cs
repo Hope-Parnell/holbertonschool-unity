dca35a7a0538f22a2aeb030fdfcc437b0d693063
Hope-Parnell <parnell.hope.c@gmail.com>
2022-04-04 15:54:18 -0500

    Camera rotate only on right click

--- 0x00-unity-assets_models_textures/Assets/Scripts/CameraController.cs
+++ 0x00-unity-assets_models_textures/Assets/Scripts/CameraController.cs
@@ -1,19 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class CameraController : MonoBehaviour
 {
-    // Camera control being done with Cinemachine
-    // Start is called before the first frame update
-    void Start()
-    {
-
+    void Start(){
+        CinemachineCore.GetInputAxis = GetAxisCustom;
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+    public float GetAxisCustom(string axisName){
+        if(axisName == "Mouse X"){
+            if (Input.GetMouseButton(1)){
+                return UnityEngine.Input.GetAxis("Mouse X");
+            } else{
+                return 0;
+            }
+        }
+        else if (axisName == "Mouse Y"){
+            if (Input.GetMouseButton(1)){
+                return UnityEngine.Input.GetAxis("Mouse Y");
+            } else{
+                return 0;
+            }
+        }
+        return UnityEngine.Input.GetAxis(axisName);
     }
 }
