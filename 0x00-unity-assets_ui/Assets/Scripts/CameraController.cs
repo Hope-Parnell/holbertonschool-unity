5a6a25376f13f135340c847dc9765f0f652c9846
Hope-Parnell <parnell.hope.c@gmail.com>
2022-04-15 21:50:57 -0500

    Updated Options Menu

--- 0x00-unity-assets_ui/Assets/Scripts/CameraController.cs
+++ 0x00-unity-assets_ui/Assets/Scripts/CameraController.cs
@@ -5,20 +5,21 @@ using Cinemachine;
 
 public class CameraController : MonoBehaviour
 {
+    public bool isInverted;
     void Start(){
         CinemachineCore.GetInputAxis = GetAxisCustom;
     }
     public float GetAxisCustom(string axisName){
         if(axisName == "Mouse X"){
-            if (Input.GetMouseButton(1)){
+            if (Input.GetMouseButton(1) || PlayerPrefs.GetInt("freeCam", 0) == 1){
                 return UnityEngine.Input.GetAxis("Mouse X");
             } else{
                 return 0;
             }
         }
         else if (axisName == "Mouse Y"){
-            if (Input.GetMouseButton(1)){
-                return UnityEngine.Input.GetAxis("Mouse Y");
+            if (Input.GetMouseButton(1) || PlayerPrefs.GetInt("freeCam", 0) == 1){
+                return PlayerPrefs.GetInt("yInvert", 1) * UnityEngine.Input.GetAxis("Mouse Y");
             } else{
                 return 0;
             }
--- 0x00-unity-assets_ui/Assets/Scripts/OptionsMenu.cs
+++ 0x00-unity-assets_ui/Assets/Scripts/OptionsMenu.cs
@@ -6,18 +6,45 @@ using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour
 {
+    public Toggle invertY;
+    public Toggle freeCam;
     // Start is called before the first frame update
     void Start()
     {
+        if (PlayerPrefs.HasKey("yInvert")){
+            if (PlayerPrefs.GetInt("yInvert") == -1)
+                invertY.isOn = true;
+            else
+                invertY.isOn = false;
+        }
+        else{
+            PlayerPrefs.SetInt("yInvert", 1);
+        }
+        if (PlayerPrefs.HasKey("freeCam")){
+            if (PlayerPrefs.GetInt("freeCam") == 1)
+                freeCam.isOn = true;
+            else
+                freeCam.isOn = false;
+        }
+        else{
+            PlayerPrefs.SetInt("freeCam", 0);
+        }
 
     }
 
     // Update is called once per frame
-    void Update()
-    {
-
-    }
     public void Back(){
+        SceneManager.LoadScene(PlayerPrefs.GetString("Prev"));
+    }
+    public void Apply(){
+        if (invertY.isOn)
+            PlayerPrefs.SetInt("yInvert", -1);
+        else
+            PlayerPrefs.SetInt("yInvert", 1);
+        if (freeCam.isOn)
+            PlayerPrefs.SetInt("freeCam", 1);
+        else
+            PlayerPrefs.SetInt("freeCam", 0);
         SceneManager.LoadScene(PlayerPrefs.GetString("Prev"));
     }
 }
