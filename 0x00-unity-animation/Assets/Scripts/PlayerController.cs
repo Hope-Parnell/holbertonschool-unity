3f5fb3eaa4dc92384bd58ed501ac9c4d675456a1
Hope-Parnell <parnell.hope.c@gmail.com>
2022-04-28 14:18:50 -0500

    Add run animation

--- 0x00-unity-animation/Assets/Scripts/PlayerController.cs
+++ 0x00-unity-animation/Assets/Scripts/PlayerController.cs
@@ -19,6 +19,7 @@ public class PlayerController : MonoBehaviour
     public LayerMask Platforms;
     bool isGrounded;
     public float jumpHeight = 3f;
+    public Animator animator;
 
     // Update is called once per frame
     void Update()
@@ -36,11 +37,15 @@ public class PlayerController : MonoBehaviour
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
         if (direction.magnitude >= 0.1f) {
+            animator.SetBool("isRunning", true);
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity,turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+        else {
+            animator.SetBool("isRunning", false);
         }
         if (Input.GetButtonDown("Jump") && isGrounded){
             velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
